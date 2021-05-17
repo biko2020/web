5ea6c9de81a7f82f5d1c6a6f81826f121b72674b
AITOUFKIR <brahimlion38@hotmail.com>
2021-05-17 18:26:05 +0100

    using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About-Us</title>
-</head>
-<body>
+@extends('index')
+@section('content')
+
+
     <p>Built with &hearts; by Ait oufkir Brahim</p>
     <p><a href="/"> page d'accueil</a></p>
-    <footer>
-        <p>&copy; Copyright {{ date('Y') }}</p>
-    </footer>
-</body>
-</html>
+
+
+@endsection
