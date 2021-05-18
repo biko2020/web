01be57a34e89381b46b8b239eb1adb7dcbb0df2c
AITOUFKIR <brahimlion38@hotmail.com>
2021-05-18 23:38:56 +0100

    Add mages to cdn servce Amazon S3

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,7 +6,12 @@
 
 @section('content')
 
-    <img src="/images/canada.png" alt="drapeau Canada">
+     <!--Pourquoi Utiliser asset pour nos fichier Statique-->
+
+     <!--l'utilisation de la fonction asset c'est just pour stock les images
+     dans le serveur le plus proche du client base sur le principe de CDN Content Delivery Network-->
+
+    <img src="{{asset('images/canada.png')}}" alt="drapeau Canada">
     <p>Built with &hearts; by Ait oufkir Brahim</p>
 
     <p><a href="{{route('page_home')}}"> page d'accueil</a></p>
