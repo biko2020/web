def8852ba0a3bcfb6f89b66c3f7ac2820f88588d
AITOUFKIR <brahimlion38@hotmail.com>
2021-05-20 02:58:37 +0100

    Route:: view insted of Route :: get

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,23 @@ use Illuminate\Support\Facades\Route;
 // nome de notre route soit en le met au debut
 //Route::name->('page_home')get('/', function()
 
+/* old function
 Route::get('/', function(){ 
     return view('pages.home');
 })-> name('page_home');
 
 
-
 Route::get('/Notre page about Us', function(){
     return view('pages.about');
 })->name('page_about');// nome de notre route soit en le met en bas
+*/
 
+/** ----------Code PHP-------------------- ***/
+/* Route::get('/', fn() => view('pages.home')->name('page_home'));
+   Route::get('/About-us', fn() => view('pages.about')->name('page_about'));
+*/
+
+/**------------- Code Laravel-------------- ***/
+
+Route::view('/', 'pages.home')->name('page_home');
+Route::view('/About-Us', 'pages.about')->name('page_about');
